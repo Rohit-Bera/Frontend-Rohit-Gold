dfad1e40894723f8e26cbd2345cd060f3ebfe6f9
rohit <rohitnitaibera@gmail.com>
2021-08-01 21:48:54 +0530

    error updated

--- src/Components/App.js
+++ src/Components/App.js
@@ -51,6 +51,151 @@ const App = () => {
         <Switch>
           {/* admin part */}
           <Route
+            path="/adminpannel"
+            exact
+            component={(AdminPannel, Adminhero)}
+          />
+
+          <Route path="/adminpannel/profile" exact component={Adminprofile} />
+          <Route path="/adminpannel/users" exact component={Adminusers} />
+
+          <Route
+            path="/adminpannel/products/collection"
+            exact
+            component={Productcollection}
+          />
+          <Route
+            path="/adminpannel/products/addProduct"
+            exact
+            component={Productaddproduct}
+          />
+          <Route
+            path="/adminpannel/products/allProduct"
+            exact
+            component={Productallproduct}
+          />
+          <Route
+            path="/adminpannel/products/updateProduct"
+            exact
+            component={Updateproduct}
+          />
+          <Route path="/adminpannel/feedback" exact component={AdminFeedback} />
+
+          <Route
+            path="/adminpannel/orders/allorders"
+            exact
+            component={OrdersAllOrders}
+          />
+          <Route
+            path="/adminpannel/orders/dispacth"
+            exact
+            component={OrderDelievered}
+          />
+          <Route
+            path="/adminpannel/orders/notdispatch"
+            exact
+            component={OrderUndelievered}
+          />
+          <Route
+            path="/adminpannel/UserChats"
+            exact
+            component={AdminMessenger}
+          />
+
+          {/* market path */}
+          <Route path="/" exact component={Marketplace} />
+          <Route path="/home" exact component={Marketplace} />
+
+          <Route path="/home/rose-gold" exact component={Marketproduct} />
+          <Route
+            path="/home/wedding-jwellery"
+            exact
+            component={Marketproduct}
+          />
+          <Route
+            path="/home/platinum-collection"
+            exact
+            component={Marketproduct}
+          />
+          <Route
+            path="/home/mangalsutra-collection"
+            exact
+            component={Marketproduct}
+          />
+          <Route
+            path="/home/love-alphabet-collection"
+            exact
+            component={Marketproduct}
+          />
+          <Route
+            path="/home/mangalsutra-ring-collection"
+            exact
+            component={Marketproduct}
+          />
+          <Route
+            path="/home/men-platinum-collection"
+            exact
+            component={Marketproduct}
+          />
+          <Route path="/home/zoul-collection" exact component={Marketproduct} />
+          <Route path="/home/envy-collection" exact component={Marketproduct} />
+
+          <Route
+            path="/home/birthday-collection"
+            exact
+            component={Marketproduct}
+          />
+          <Route
+            path="/home/engagement-collection"
+            exact
+            component={Marketproduct}
+          />
+          <Route
+            path="/home/festive-collection"
+            exact
+            component={Marketproduct}
+          />
+          <Route
+            path="/home/mothers-day-collection"
+            exact
+            component={Marketproduct}
+          />
+          <Route path="/home/allproducts" exact component={Allproducts} />
+
+          <Route path="/home/searchproduct" exact component={Searchproduct} />
+
+          {/* users path */}
+          <Route path="/LoginSignup" exact component={Forms} />
+          <Route path="/Wishlist" exact component={Wishlist} />
+          <Route path="/Cart" exact component={Cart} />
+          {/* forms */}
+          <Route path="/Login" exact component={Login} />
+          <Route path="/Signup" exact component={Signup} />
+          {/* myprofile */}
+          <Route
+            path="/Myprofile"
+            exact
+            component={(ProfileHeader, Overview)}
+          />
+          <Route path="/Myprofile/Overview" exact component={Overview} />
+          <Route path="/Myprofile/Profile" exact component={Myprofile} />
+          <Route path="/Myprofile/Edit" exact component={Editform} />
+          <Route path="/Myprofile/Wishlist" exact component={Wishlist} />
+          <Route path="/Myprofile/Myorders" exact component={Myorders} />
+          <Route path="/Myprofile/Toorder" exact component={Cart} />
+
+          <Route
+            path="/home/productcollection/viewproduct"
+            exact
+            component={ViewProduct}
+          />
+
+          {/* user chat */}
+          <Route path="/help/chat-with-helpCenter" exact component={Chat} />
+
+          {/* extra back log*/}
+
+          <Route
             path="https://rohits-goldapp.netlify.app/adminpannel"
             exact
             component={(AdminPannel, Adminhero)}
@@ -115,7 +260,11 @@ const App = () => {
           />
 
           {/* market path */}
-          <Route path="/" exact component={Marketplace} />
+          <Route
+            path="https://rohits-goldapp.netlify.app/"
+            exact
+            component={Marketplace}
+          />
           <Route
             path="https://rohits-goldapp.netlify.app/home"
             exact
@@ -276,6 +425,7 @@ const App = () => {
             exact
             component={Chat}
           />
+
           {/* admin */}
           {/* <Routr path="" exact component={} />
           <Route path="" exact component={} /> */}
